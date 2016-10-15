32ddfe0063a143552e7bc0eb9bbedead4922083d
iniezen <iniezen@gmail.com>
2016-10-15 13:53:44 -0700

    models and migration files

---
+++ models/tradeoffer.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var TradeOffer = sequelize.define('TradeOffer', {
+    status: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+       TradeOffer.hasMany(models.Notification);
+       // the next associations need to be reviewed please
+       // TradeOffer.hasOne(models.User); user 1
+       // TradeOffer.hasOne(models.User); user 2
+       // TradeOffer.hasOne(models.Product); product 1
+       // TradeOffer.hasOne(models.Product); product 2
+
+
+      }
+    }
+  });
+  return TradeOffer;
+};
