5b0de46dfb545e0db2f73bfdfafb062a73b22bf8
Andrew Kil <andrewkil86@gmail.com>
2016-10-21 06:32:20 -0700

    adding some aesthetic features

--- public/assets/javascript/logo-hover.js
+++ public/assets/javascript/logo-hover.js
@@ -5,4 +5,12 @@ $("document").ready(function(){
     $(".logo-hover").mouseleave(function(){       
         $(this).attr('src','assets/img/logo-white.png');      
     }); 
+	 $('.main').hover(function() {
+	        $(this).find('.main-area').hide();
+	        $(this).find('.main-description').show();
+	    }, function() {
+	        $(this).find('.main-description').hide();
+	        $(this).find('.main-area').show();
+	});
+   
 });
