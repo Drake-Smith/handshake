fb488a8022dd9a313cd67f16d46ea6ccf43b80b2
Andrew Kil <andrewkil86@gmail.com>
2016-10-21 21:09:20 -0700

    merging

---
+++ public/assets/javascript/googlemap.js
@@ -0,0 +1,4 @@
+var adress1;
+var adress2;
+
+var queryURL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + adress1 + "&destinations=" + adress2 + "&key=AIzaSyCzXSsuD-ES0dD0fXLOY-CVqH0nksSIiPo";
